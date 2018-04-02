c06744d41792f33fdf2d4e667fd470f5d5dcd6ce
Eduardo <eduardo.buganemi@gmail.com>
2018-04-02 18:58:32 -0400

    funcionando templates

--- meublog/posts/views.py
+++ meublog/posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
 def index(request):
-    return render(request, 'blog.html')
+    posts = Post.objects.all().order_by('created_date')
+    return render(request, 'blog.html', {'posts' : posts})
