9d4d5c91618607c84d33d15c55c5502a39762d42
botelhoeduardo <eduardo.buganemi@gmail.com>
2018-04-02 01:25:51 -0400

    mexendo em views

--- meublog/posts/views.py
+++ meublog/posts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá, estamos no index do app posts")
+    return render(request, 'blog.html')
