c9ba53539ce6460ee30a8f8573f66ff426c780cd
botelhoeduardo <eduardo.buganemi@gmail.com>
2018-04-03 14:55:14 -0400

    arrumando post novo

--- meublog/posts/views.py
+++ meublog/posts/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.all().order_by('created_date')
     return render(request, 'blog.html', {'posts' : posts})
+
+def post_novo(request):
+    form = PostForm()
+    return render(request, 'novo.html', {'form' : form})
