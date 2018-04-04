8365d7129e7d4cfed38e46bc3227aeebcb73c5c7
botelhoeduardo <eduardo.buganemi@gmail.com>
2018-04-03 23:07:48 -0400

    fim da disciplina

--- meublog/posts/views.py
+++ meublog/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post
@@ -9,5 +9,14 @@ def index(request):
     return render(request, 'blog.html', {'posts' : posts})
 
 def post_novo(request):
-    form = PostForm()
-    return render(request, 'novo.html', {'form' : form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+        return render(request, 'novo.html', {'form' : form})
